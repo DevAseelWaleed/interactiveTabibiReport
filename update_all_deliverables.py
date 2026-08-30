d06434fa73c218f4840d5df5e9728ea6e7952e2a
Aseel <aseelwaleed983@gmail.com>
2026-08-30 21:55:07 +0300

    Keep only index.html and assets directory

--- update_all_deliverables.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys, os, subprocess
-
-sys.stdout.reconfigure(encoding='utf-8')
-base_dir = r"e:\Work\زبون تقرير نصف سنوي طبيبي"
-
-print("Starting generation of all updated deliverables...")
